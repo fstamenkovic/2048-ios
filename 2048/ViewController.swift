ad26ea2ea75e2f5d8ddb0d848353244d7c8e4d5c
fstamenkovic <fstamenkovic@ucdavis.edu>
2020-03-28 19:27:14 -0500

    Basic initialization

--- 2048/ViewController.swift
+++ 2048/ViewController.swift
@@ -10,9 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label00: UILabel!
+    @IBOutlet weak var label01: UILabel!
+    @IBOutlet weak var label02: UILabel!
+    @IBOutlet weak var label10: UILabel!
+    @IBOutlet weak var label11: UILabel!
+    @IBOutlet weak var label12: UILabel!
+    @IBOutlet weak var label20: UILabel!
+    @IBOutlet weak var label21: UILabel!
+    @IBOutlet weak var label22: UILabel!
+    
+    var fields = [[UILabel]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        initialize_game()
+        
+    }
+    
+    func initialize_game(){
+        populate_fields()
+        initialize_fields()
+    }
+    
+    func populate_fields(){
+        fields.append([label00, label01, label02])
+        fields.append([label10, label11, label12])
+        fields.append([label20, label21, label22])
+    }
+    
+    func initialize_fields(){
+        
+        // set all cells to empty and grey
+        for row in fields{
+            for cell in row{
+                cell.backgroundColor = UIColor.lightGray
+                cell.text = ""
+            }
+        }
+        
+        // populate a random field with a "2"
+        let num1 = Int.random(in: 0 ... 2)
+        let num2 = Int.random(in: 0 ... 2)
+        
+        fields[num1][num2].text = String(2)
+        
+        var num3 = Int.random(in: 0 ... 2)
+        var num4 = Int.random(in: 0 ... 2)
+        
+        while num1 == num3 && num2 == num4 {
+            num3 = Int.random(in: 0 ... 2)
+            num4 = Int.random(in: 0 ... 2)
+        }
+        
+        fields[num3][num4].text = String(4)
     }
 
    
